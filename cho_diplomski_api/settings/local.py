fd7a2be0362bfb7b89cef16c784a0667b125d1d0
Filip Jukic <filip@jukic.me>
2015-04-23 17:54:43 +0200

    initial commit

---
+++ cho_diplomski_api/settings/local.py
@@ -0,0 +1,33 @@
+from .base import *
+
+
+ADMINS = (
+    ('Filip Jukić', 'filip@jukic.me'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cho_diplomski_api',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+# You might want to use sqlite3 for testing in local as it's much faster.
+if len(sys.argv) > 1 and 'test' in sys.argv[1]:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/tmp/cho_diplomski_api_test.db',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
+    }
